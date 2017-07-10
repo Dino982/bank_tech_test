5b3690ee95d150a3b44f5be6491130c43ab111ea
Andrew Clarke <andrewclarke982@gmail.com>
2017-07-10 18:56:21 +0100

    Account has an initial balance of zero

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,7 +3,11 @@ require 'account'
 describe Account do
   subject(:account) { described_class.new }
 
-  it 'should exist' do
+  it 'Should exist' do
     expect(account).to be_truthy
+  end
+
+  it 'Starts with an initial balance of zero' do
+    expect(account.balance).to eq(0)
   end
 end
